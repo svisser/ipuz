4ab36c4e933147bd5560baf88cc8b0cfeb856a42
Simeon Visser <simeon87@gmail.com>
2014-04-29 17:03:50 +0100

    Added relevant classifiers to setup.py

--- setup.py
+++ setup.py
@@ -9,5 +9,14 @@ setup(
     url="https://github.com/svisser/ipuz",
     install_requires=[
         'six==1.6.1'
+    ],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.3",
+        "Topic :: Games/Entertainment :: Puzzle Games",
     ]
 )
