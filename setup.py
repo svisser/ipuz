89b46ea1669ad2ef283e701f168769c768cb2900
Simeon Visser <simeon87@gmail.com>
2014-04-30 11:41:28 +0100

    Added README as long_description in setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,15 @@
 from setuptools import setup
 
 
+with open('README.md') as readme_file:
+    long_description = readme_file.read()
+
+
 setup(
     name="ipuz",
     version="0.1",
     description="Python library for reading and writing ipuz puzzle files",
+    long_description=long_description,
     author="Simeon Visser",
     url="https://github.com/svisser/ipuz",
     install_requires=[
