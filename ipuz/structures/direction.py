e120b6cf884dbda9d12bd15fcbf9c009f1fd6434
Simeon Visser <simeon87@gmail.com>
2014-05-05 01:17:59 +0200

    Minor change in logic for direction name calculation

--- ipuz/structures/direction.py
+++ ipuz/structures/direction.py
@@ -2,9 +2,9 @@
 
 def validate_direction(direction):
     splitted = direction.split(':')
-    direction_name = direction
     if len(splitted) > 2:
         return False
+    direction_name = direction
     if len(splitted) <= 2:
         direction_name = splitted[0]
     return direction_name in (
