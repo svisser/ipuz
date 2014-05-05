802bf3a47ded6acebe6ff277c308262994b03288
Simeon Visser <simeon87@gmail.com>
2014-05-05 23:26:14 +0200

    Added basic validation to some Acrostic puzzle fields

--- ipuz/puzzlekinds/acrostic.py
+++ ipuz/puzzlekinds/acrostic.py
@@ -1,2 +1,11 @@
+from ipuz.structures import (
+    validate_crosswordvalues,
+    validate_labeledcell,
+)
+from ipuz.validators import validate_list_of_lists
 
-IPUZ_ACROSTIC_VALIDATORS = {}
+
+IPUZ_ACROSTIC_VALIDATORS = {
+    "puzzle": (validate_list_of_lists, "LabeledCell", validate_labeledcell),
+    "solution": validate_crosswordvalues,
+}
