c4e5a1542275c041387da2cc6de5897d58e42227
Simeon Visser <simeon87@gmail.com>
2014-05-05 02:42:00 +0200

    Refactored more code to use validate_dict function

--- ipuz/structures/sudokugiven.py
+++ ipuz/structures/sudokugiven.py
@@ -2,6 +2,7 @@ import six
 
 from ipuz.exceptions import IPUZException
 from ipuz.structures import validate_stylespec
+from ipuz.validators import validate_dict
 
 
 def validate_sudokugiven(field_data):
@@ -9,18 +10,19 @@ def validate_sudokugiven(field_data):
         if given is not None and type(given) is not int and not isinstance(given, six.string_types):
             return False
         return True
+    
+    def validate_style_key(value):
+        try:
+            validate_stylespec(value)
+        except IPUZException:
+            return False
+        return True
+    
     if field_data is not None and type(field_data) is not int and not isinstance(field_data, six.string_types):
         return False
     if isinstance(field_data, dict):
-        if not field_data:
-            return False
-        if not all(key in ("given", "style") for key in field_data):
-            return False
-        if "given" in field_data and not validate_given(field_data["given"]):
-            return False
-        if "style" in field_data:
-            try:
-                validate_stylespec(field_data["style"])
-            except IPUZException:
-                return False
+        return validate_dict(field_data, {
+            "given": validate_given,
+            "style": validate_style_key,
+        })
     return True
--- ipuz/structures/sudokuvalue.py
+++ ipuz/structures/sudokuvalue.py
@@ -2,6 +2,7 @@ import six
 
 from ipuz.exceptions import IPUZException
 from ipuz.structures import validate_stylespec
+from ipuz.validators import validate_dict
 
 
 def validate_sudokuvalue(field_data):
@@ -9,19 +10,19 @@ def validate_sudokuvalue(field_data):
         if value is not None and type(value) is not int and not isinstance(value, six.string_types):
             return False
         return True
+    
+    def validate_style_key(value):
+        try:
+            validate_stylespec(value)
+        except IPUZException:
+            return False
+        return True
 
     if field_data is not None and type(field_data) is not int and not isinstance(field_data, six.string_types):
         return False
     if isinstance(field_data, dict):
-        if not field_data:
-            return False
-        if not all(key in ("value", "style") for key in field_data):
-            return False
-        if "value" in field_data and not validate_value(field_data["value"]):
-            return False
-        if "style" in field_data:
-            try:
-                validate_stylespec(field_data["style"])
-            except IPUZException:
-                return False
+        return validate_dict(field_data, {
+            "value": validate_value,
+            "style": validate_style_key,
+        })
     return True
