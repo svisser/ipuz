f376cc0a11e81311a3be02150db281ae15f8dbb3
Simeon Visser <simeon87@gmail.com>
2014-05-06 21:53:23 +0200

    Added validation to some Fill fields

--- ipuz/puzzlekinds/fill.py
+++ ipuz/puzzlekinds/fill.py
@@ -1,2 +1,17 @@
+from ipuz.structures import (
+    validate_crosswordvalues,
+)
+from ipuz.validators import (
+    validate_dict_of_strings,
+    validate_list_of_strings,
+    validate_string,
+)
 
-IPUZ_FILL_VALIDATORS = {}
+
+IPUZ_FILL_VALIDATORS = {
+    "start": validate_crosswordvalues,
+    "solution": validate_crosswordvalues,
+    "answer": validate_string,
+    "answers": validate_list_of_strings,
+    "misses": validate_dict_of_strings,
+}
